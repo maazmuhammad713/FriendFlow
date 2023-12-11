89a93ad79dce27635cfb20d14cbe1f537d75a08e
maazmuhammad713 <maazmuhammad713@gmail.com>
2023-12-11 15:02:17 +0500

    commit friendflow

---
+++ client/src/components/Friend.jsx
@@ -0,0 +1,109 @@
+import { PersonAddOutlined, PersonRemoveOutlined } from "@mui/icons-material";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setFriends } from "state";
+import FlexBetween from "./FlexBetween";
+import UserImage from "./UserImage";
+import toast from "react-hot-toast";
+
+const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { _id } = useSelector((state) => state.user);
+  const token = useSelector((state) => state.token);
+  const friends = useSelector((state) => state.user.friends);
+
+  const { palette } = useTheme();
+  const primaryLight = palette.primary.light;
+  const primaryDark = palette.primary.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
+  const isFriend = friends.find((friend) => friend._id === friendId);
+
+  const patchFriend = async () => {
+    if (_id === friendId) {
+      toast.error("Cannot add or remove yourself as a friend. :(");
+      return;
+    }
+    const response = await fetch(
+      `http://localhost:3001/users/${_id}/${friendId}`,
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (response.ok) {
+      const data = await response.json();
+      dispatch(setFriends({ friends: data }));
+      if (isFriend) {
+        toast.success(`Unfollowed ${name}`, {
+          position: "top-right",
+          style: {
+            fontSize: "1.2rem", // Adjust the font size as needed
+            padding: "20px", // Adjust the padding as needed
+            marginTop: "35px", // Adjust the margin as needed
+          },
+        });
+      } else {
+        toast.success(`Followed ${name}`, {
+          position: "top-right",
+          style: {
+            fontSize: "1rem", // Adjust the font size as needed
+            padding: "20px", // Adjust the padding as needed
+            marginTop: "35px", // Adjust the margin as needed
+          },
+        });
+      }
+    } else {
+      toast.error("Failed to add/remove friend:", response.statusText);
+    }
+  };
+
+  return (
+    <FlexBetween>
+      <FlexBetween gap="1rem">
+        <UserImage image={userPicturePath} size="55px" />
+        <Box
+          onClick={() => {
+            navigate(`/profile/${friendId}`);
+            navigate(0);
+          }}
+        >
+          <Typography
+            color={main}
+            variant="h5"
+            fontWeight="500"
+            sx={{
+              "&:hover": {
+                color: palette.primary.light,
+                cursor: "pointer",
+              },
+            }}
+          >
+            {name}
+          </Typography>
+          <Typography color={medium} fontSize="0.75rem">
+            {subtitle}
+          </Typography>
+        </Box>
+      </FlexBetween>
+      <IconButton
+        onClick={() => patchFriend()}
+        sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
+      >
+        {isFriend ? (
+          <PersonRemoveOutlined sx={{ color: primaryDark }} />
+        ) : (
+          <PersonAddOutlined sx={{ color: primaryDark }} />
+        )}
+      </IconButton>
+    </FlexBetween>
+  );
+};
+
+export default Friend;
