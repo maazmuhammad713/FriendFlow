29f5ac77d24635276af58a28575b62000fa95d43
maazmuhammad713 <maazmuhammad713@gmail.com>
2023-12-11 15:54:11 +0500

    a

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: ["https://friend-flow.vercel.app/"],
+    origin: ["https://friend-flow.vercel.app/_logs"],
     methods: ["POST", "GET"],
     credentials: true,
   })
